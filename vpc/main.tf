775b8e87bd3129c716446e33f238123f8714e1af
NamSee04 <nghainam2903@gmail.com>
2024-10-03 23:31:50 +0700

    tf-fmt check

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,115 +1,115 @@
 resource "aws_vpc" "vpc" {
-    cidr_block           = var.vpc_cidr_block
-    enable_dns_hostnames = true
+  cidr_block           = var.vpc_cidr_block
+  enable_dns_hostnames = true
 
-    tags = {
-        "Name" = "Group-20"
-    }
+  tags = {
+    "Name" = "Group-20"
+  }
 }
 
 resource "aws_security_group" "default" {
-    vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id
 
-    ingress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1" // Allow all traffic
-        cidr_blocks  = ["10.0.0.0/16"] // Adjust this based on your CIDR block
-    }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
 
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1" // Allow all outbound traffic
-        cidr_blocks  = ["0.0.0.0/0"] // Allow all outbound
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "default"
-    }
+  tags = {
+    Name = "default"
+  }
 }
 
 resource "aws_subnet" "private_subnet" {
-    count = length(var.private_subnet)
+  count = length(var.private_subnet)
 
-    vpc_id            = aws_vpc.vpc.id
-    cidr_block        = var.private_subnet[count.index]
-    availability_zone = var.availability_zone[count.index % length(var.availability_zone)]
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.private_subnet[count.index]
+  availability_zone = var.availability_zone[count.index % length(var.availability_zone)]
 
-    tags = {
-        "Name" = "private-subnet"
-    }
+  tags = {
+    "Name" = "private-subnet"
+  }
 }
 
 resource "aws_subnet" "public_subnet" {
-    count = length(var.public_subnet)
+  count = length(var.public_subnet)
 
-    vpc_id            = aws_vpc.vpc.id
-    cidr_block        = var.public_subnet[count.index]
-    availability_zone = var.availability_zone[count.index % length(var.availability_zone)]
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = var.public_subnet[count.index]
+  availability_zone = var.availability_zone[count.index % length(var.availability_zone)]
 
-    tags = {
-        "Name" = "public-subnet"
-    }
+  tags = {
+    "Name" = "public-subnet"
+  }
 }
 
 resource "aws_internet_gateway" "ig" {
-    vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id
 
-    tags = {
-        "Name" = "custom"
-    }
+  tags = {
+    "Name" = "custom"
+  }
 }
 
 resource "aws_route_table" "public" {
-    vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.ig.id
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
+  }
 
-    tags = {
-        "Name" = "public"
-    }
+  tags = {
+    "Name" = "public"
+  }
 }
 
 resource "aws_route_table_association" "public_association" {
-    for_each       = { for k, v in aws_subnet.public_subnet : k => v }
-    subnet_id      = each.value.id
-    route_table_id = aws_route_table.public.id
+  for_each       = { for k, v in aws_subnet.public_subnet : k => v }
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_eip" "nat" {
-    vpc = true
+  vpc = true
 }
 
 resource "aws_nat_gateway" "public" {
-    depends_on = [aws_internet_gateway.ig]
+  depends_on = [aws_internet_gateway.ig]
 
-    allocation_id = aws_eip.nat.id
-    subnet_id     = aws_subnet.public_subnet[0].id
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_subnet[0].id
 
-    tags = {
-        Name = "Public NAT"
-    }
+  tags = {
+    Name = "Public NAT"
+  }
 }
 
 resource "aws_route_table" "private" {
-    vpc_id = aws_vpc.vpc.id
+  vpc_id = aws_vpc.vpc.id
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_nat_gateway.public.id
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.public.id
+  }
 
-    tags = {
-        "Name" = "private"
-    }
+  tags = {
+    "Name" = "private"
+  }
 }
 
 resource "aws_route_table_association" "public_private" {
-    for_each       = { for k, v in aws_subnet.private_subnet : k => v }
-    subnet_id      = each.value.id
-    route_table_id = aws_route_table.private.id
+  for_each       = { for k, v in aws_subnet.private_subnet : k => v }
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private.id
 }
