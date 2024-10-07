acf238597a20eacba204120df5dd51109ee87fda
NamSee04 <nghainam2903@gmail.com>
2024-10-07 21:24:30 +0700

    ex2

--- main.tf
+++ main.tf
@@ -10,3 +10,15 @@ module "vpc" {
   public_subnet     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   availability_zone = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
+
+module "ec2" {
+  source = "./ec2"
+
+  vpc_id                = module.vpc.vpc_id
+  public_subnet_id      = module.vpc.public_subnet_id
+  private_subnet_id     = module.vpc.private_subnet_id
+  public_instance_ami   = "ami-032346ab877c418af"
+  private_instance_ami  = "ami-0f59d29a0ef46a444"
+  public_instance_type  = "m5.large"
+  private_instance_type = "t2.micro"
+}
